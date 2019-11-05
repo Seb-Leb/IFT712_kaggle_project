745c40b8ee8a9d975b3bab7fdbfb3efa6d188134
Seb-Leb <seb@leb.cat>
2019-11-05 12:52:35 -0500

    adjusted datatypes

---
+++ data_management.py
@@ -0,0 +1,14 @@
+
+def parse_csv(file_path):
+    with open(file_path, 'r') as f:
+        for n,l in enumerate(f):
+            ls = l.strip().split(',')
+            if n==0:
+                keys = ls
+                continue
+            line = dict(zip(keys, ls))
+            for k in line.keys():
+                if k not in ('id', 'diagnosis'):
+                    line[k] = float(line[k])
+            data.append(line)
+    return data
