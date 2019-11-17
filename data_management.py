3f6bacded72dc73b650eb1cf8520ba651e39fd20
Seb-Leb <seb@leb.cat>
2019-11-17 13:39:42 -0500

    more data exploration, PCAs..

--- data_management.py
+++ data_management.py
@@ -1,10 +1,14 @@
 def parse_csv(file_path):
-    with open(file_path, 'r') as f:
+    data = []
+    with open('data.csv', 'r') as f:
         for n,l in enumerate(f):
             ls = l.strip().split(',')
             if n==0:
-                keys = ls
+                keys = [x.replace('"','') for x in ls]
+                features = keys[2:-1]
                 continue
             line = dict(zip(keys, ls))
+            for f in features:
+                line[f] = float(line[f])
             data.append(line)
     return data
