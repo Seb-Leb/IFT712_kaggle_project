e00dd79a7a85b82f269f2369ffce4fe34de22826
Seb-Leb <seb@leb.cat>
2019-11-19 11:38:22 -0500

    added models, some more structure...

--- models.py
+++ models.py
@@ -1,4 +1,32 @@
 import numpy as np
+from sklearn import svm, neural_network
 
 class model:
     def __int__(self, ):
+        '''
+        instantiate an ML model (SVM_rbf, SVM_sigmoid, MLP, ***, ***)
+        '''
+        pass
+
+    def train(self, x_train, t_train):
+        # SVM RBF kernel
+        clf = svm.SVC(gamma=self.gamma)
+        self.trained_model = clf.fit(x_train, t_train)
+
+        # SVM sigmoid kernel
+
+        # Multi layer perceptron
+
+    def predict(self, x):
+        pass
+
+    def hyperpar_tuning():
+        '''
+
+        '''
+        def cross_val():
+            '''
+
+            '''
+        pass
+    pass
