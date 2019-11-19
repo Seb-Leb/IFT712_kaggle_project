bcc91fdcbe9483dbefd3fbcd7fdede2f13fc7b11
Seb-Leb <seb@leb.cat>
2019-11-19 11:21:39 -0500

    sklearn train test split

--- data_management.py
+++ data_management.py
@@ -1,10 +1,15 @@
 import numpy
+from sklearn.model_selection import train_test_split
 
 
 class data_manager:
-    def __init__(self, dataset_path, train_test_split_ratio):
+    def __init__(self, dataset_path, test_size=0.3):
+        '''
+        dataset path: relative path to the data.csv file from the winsconsin breast cancer dataset.
+        test_size: value between ]0,1[ representing the fraction of points to be used for the test set.
+        '''
         self.dataset_path = dataset_path
-        self.train_test_split_ratio = train_test_split_ratio
+        self.test_size = test_size
 
     def parse_csv(file_path):
         '''
@@ -23,9 +28,15 @@ class data_manager:
                     if k not in ('id', 'diagnosis'):
                         line[k] = float(line[k])
                 data.append(line)
-        return data
+        X = np.array([[v for k,v in x.items() if k not in {'id', 'diagnosis'}] for x in data])
+        T = np.array([1. if x['diagnosis']=='M' else 0. for x in data])
+        return X, T
 
-    def split_traning_test(self):
+    def split_traning_test(self, X, T):
+        x_train, x_test, t_train, t_test = train_test_split(X, T, test_size=self.test_size)
         return x_train, t_train, x_test, t_test
 
-    def get_data(self, ):
+    def get_data(self):
+        data = self.parse_csv(self.dataset_path)
+        return self.split_trainning_test(data)
+
