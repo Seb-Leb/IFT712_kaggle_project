697525b881a730d5a0ebc0e4c3a3557b880361fe
Seb-Leb <seb@leb.cat>
2019-11-24 11:56:39 -0500

    model definitions

--- models.py
+++ models.py
@@ -1,4 +1,12 @@
+from sklearn import svm, neural_network
 import numpy as np
 
-class model:
+class Model:
     def __int__(self, ):
+        pass
+
+    def svm(self, kernel):
+        pass
+
+    def neural_net(self, n_layers, n_neurons):
+        pass
