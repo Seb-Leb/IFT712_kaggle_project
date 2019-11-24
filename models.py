80c551837a65b38b638c620702a2b4697e9e8a8c
Seb-Leb <seb@leb.cat>
2019-11-24 12:32:24 -0500

    more models dev..

--- models.py
+++ models.py
@@ -1,28 +1,32 @@
 from sklearn import svm, neural_network
 import numpy as np
-from sklearn import svm, neural_network
 
 class Model:
-    def __int__(self, ):
-        pass
-
-    def svm(self, kernel):
-        pass
-
-    def neural_net(self, n_layers, n_neurons):
+    def __int__(self, model_type, gamma='scale'):
         '''
         instantiate an ML model (SVM_rbf, SVM_sigmoid, MLP, ***, ***)
         '''
-        pass
+        self.model = model_type
+        self.gamma = gamma
 
     def train(self, x_train, t_train):
         # SVM RBF kernel
-        clf = svm.SVC(gamma=self.gamma)
-        self.trained_model = clf.fit(x_train, t_train)
+        if self.model == 'svm_rbf':
+            clf = svm.SVC(gamma=self.gamma, kernel='rbf')
+            self.trained_model = clf.fit(x_train, t_train)
 
         # SVM sigmoid kernel
+        if self.model == 'svm_sigmoid':
+            clf = svm.SVC(gamma=self.gamma, kernel='sigmoid')
+            self.trained_model = clf.fit(x_train, t_train)
 
         # Multi layer perceptron
+        # mlp = MLPClassifier(hidden_layer_sizes=(100, 100), max_iter=400, alpha=1e-4,
+        #                     solver='sgd', verbose=10, tol=1e-4, random_state=1)
+        if self.model == 'MLP':
+            mlp = neural_network.MLPClassifier(
+                    hidden_layer_sizes=(self.n_layers,),
+                    )
 
     def predict(self, x):
         pass
@@ -36,4 +40,6 @@ class Model:
 
             '''
         pass
-    pass
+
+    def bagging():
+        pass
