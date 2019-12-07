c92a15bf8877bb0060b6f1778cbb551962938a86
Seb-Leb <seb@leb.cat>
2019-12-07 10:50:08 -0500

    data update

--- display.py
+++ display.py
@@ -1,11 +1,16 @@
+import numpy as np
 from matplotlib import pyplot as plt
 
 class FigureDrawer:
     def __init__(self):
         pass
 
-    def draw_hist_array(self, X):
-        fig = plt.figure()
+    def draw_hist_array(self, X, bins=1000):
+        grid = int(np.sqrt(X.shape[0]))
+        fig, axs = plt.subplots(grid, grid, facecolor='w')
+        axs = axs.ravel()
+        for n,x in enumerate(X):
+            axs[n].hist(x, bins=bins)
         plt.show()
 
     def draw_line_plot(self, curves):
