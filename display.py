41d3333c498aff5020a482e67a5ae86decb024ab
Seb-Leb <seb@leb.cat>
2019-12-06 19:59:14 -0500

    created display class to show figures

---
+++ display.py
@@ -0,0 +1,15 @@
+from matplotlib import pyplot as plt
+
+class FigureDrawer:
+    def __init__(self):
+        pass
+
+    def draw_hist_array(self, X):
+        fig = plt.figure()
+        plt.show()
+
+    def draw_line_plot(self, curves):
+        plt.show()
+
+    def draw_scatter_plot(self, points):
+        plt.show()
