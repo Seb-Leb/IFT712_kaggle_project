bbb10bb36247e3696912f1c8d3f520bfb5f59017
Seb-Leb <seb@leb.cat>
2019-12-08 17:01:39 -0500

    metric from Lee and Lui

--- metrics.py
+++ metrics.py
@@ -2,18 +2,17 @@ import numpy as np
 
 
 class Metrics :
-    def __init__(self, nb_train, nb_test, lineairement_sep):
-        self.nb_train = nb_train
-        self.nb_test = nb_test
-        self.lineairement_sep = lineairement_sep
+    def __init__(self):
+        pass
 
-    def accuracy(t_pred, t_test):
+    def accuracy(self, t_pred, t_test):
         return sum(t_pred==t_test)/len(t_pred)
 
 
-    def ROC_curve():
-        pass
+    def Lee_Lui_score(self, t_pred, t_test):
+        return self.recall(t_pred, t_test)**2/sum(t_pred==1.)
 
-    def recall(t_pred, t_test):
+
+    def recall(self, t_pred, t_test):
         return sum(t_pred==t_test and t_pred==1.)/sum(t_test==1.)
 
